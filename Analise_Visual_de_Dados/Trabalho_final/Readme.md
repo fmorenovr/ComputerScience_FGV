297d212c9b6e15c4619613619e82403788461535
Felipe A. Moreno <fmorenovr@gmail.com>
2023-07-06 08:37:54 -0300

    server placepulse

--- Analise_Visual_de_Dados/Trabalho_final/Readme.md
+++ Analise_Visual_de_Dados/Trabalho_final/Readme.md
@@ -1,6 +1,6 @@
 **1) Installing dependencies using conda**
 
 ```
-    conda env create --name PlacePulse --file requirements.yaml 
+    conda env create --name server_PlacePulse --file requirements.yaml 
     conda env export > requirements.yml
 ```
--- Analise_Visual_de_Dados/Trabalho_final/server.py
+++ Analise_Visual_de_Dados/Trabalho_final/server.py
@@ -6,8 +6,6 @@ from PIL import Image
 
 from tqdm import tqdm
 
-import torch
-
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from sklearn.cluster import SpectralClustering
@@ -48,93 +46,19 @@ CORS(app)
 
 # load all of the data generated from preprocessing
 data_dict = joblib.load(f"{absolute_current_path}/outputs/static/data.joblib")
+data_dict["images"] = []
 images = np.array(data_dict["images"]).copy()
 features = np.array(data_dict["features"]).copy()
-labels = data_dict["label"]
+cities = data_dict["city"].copy()
+latitude = data_dict["latitude"].copy()
+longitude = data_dict["longitude"].copy()
+labels = data_dict["label"].copy()
+scores = data_dict["safety"].copy()
 
 img_url_data = [  f"http://localhost:{PORT}/static/images/{img.split('/')[-2]}/{img.split('/')[-1]}" for img in  data_dict["path"] ]
 
 # these variables will contain the clustering of channels for the different layers
 clustering_dict = {}
-
-def apply_max_pooling(images, pool_size=(4, 4)):
-    pool_images = []
-    for img in images:
-        #pooled_img = np.array([maximum_filter(ch, size=pool_size) for ch in img])
-        #pooled_img = maximum_filter(img, size=pool_size)
-        pooled_img = np.array([block_reduce(channel, pool_size, np.max) for channel in img])
-        #pooled_img = np.max(img, axis=(1, 2))
-        pool_images.append(pooled_img)
-    return np.array(pool_images)
-
-def get_umap_2D_projection(images, type_data="features"):
-
-    print("Input", images.shape)
-    if type_data=="features":
-        flattened_images = images.copy()
-    else:
-        #images_pooled = block_reduce(images, pool_size, np.max)
-        images_pooled = apply_max_pooling(images)
-        flattened_images = images_pooled.reshape(images_pooled.shape[0], -1)
-    
-    print("Flatten", flattened_images.shape)
-    # Standardize the pooled activations
-    scaler = StandardScaler()
-    scaled_activations = scaler.fit_transform(flattened_images)
-    
-    # Perform PCA for dimensionality reduction
-    #pca = PCA(n_components=50)  # Adjust the number of components as needed
-    #reduced_activations = pca.fit_transform(scaled_activations)
-    
-    # Apply UMAP for 2D projection
-    umap = UMAP(n_components=2)
-    embedding = umap.fit_transform(scaled_activations)
-    print("results embedding:", embedding.shape)
-    projection = embedding.tolist()
-    
-    return projection
-
-def get_tsne_2D_projection(images, type_data="features"):
-
-    print("Input", images.shape)
-    if type_data=="features":
-        flattened_images = images.copy()
-    else:
-        flattened_images = images.reshape(images.shape[0], -1)
-
-    print("Flatten", flattened_images.shape)
-    # Normalize the flattened images to have zero mean and unit variance (optional but recommended)
-    normalized_images = (flattened_images - np.mean(flattened_images, axis=0)) / np.std(flattened_images, axis=0)
-
-    # Apply t-SNE to the normalized flattened images
-    tsne = TSNE(n_components=2)  # You can adjust the number of components as needed
-    tsne_images = tsne.fit_transform(normalized_images)
-    print("results embedding:", tsne_images.shape)
-    projection = tsne_images.tolist()
-    
-    return projection
-
-def get_pca_2D_projection(images, type_data="features"):
-
-    print("Input", images.shape)
-    if type_data=="features":
-        flattened_images = images.copy()
-    else:
-        flattened_images = images.reshape(images.shape[0], -1)
-        flattened_images = flattened_images / 255.0
-    
-    print("Flatten", flattened_images.shape)
-    pca = PCA(n_components=2)  # Specify the desired number of components
-    pca_images = pca.fit_transform(flattened_images)
-    
-    # Access the principal components
-    principal_components = pca.components_
-
-    # Access the explained variance ratio
-    explained_variance_ratio = pca.explained_variance_ratio_
-    print("results embedding:", pca_images.shape)
-    projection = pca_images.tolist()
-    return projection
 
 @app.after_request
 def add_header(r):
@@ -145,6 +69,21 @@ def add_header(r):
     return r
 
 # IMAGES
+@app.route('/get_image_information/', methods=['GET', 'POST'])
+def get_image_information():
+
+    selected_ids = None
+    if request.method == 'POST':
+        try:
+            selected_ids =  request.get_json()["selected_ids"]
+            print("Selected samples", selected_ids)
+        except Exception as e:
+            print("ERROR", e)
+
+    img_info = [ {"id": index,"city": cit, "lat": lat, "long": long_, "label": lab, "score": scor} for index, (cit, lat, long_, lab, scor) in enumerate(zip(cities, latitude, longitude, labels, scores)) ]
+    
+    return flask.jsonify(img_info)
+
 @app.route('/get_image_label', methods=['GET', 'POST'])
 def get_image_label():
 
@@ -164,13 +103,8 @@ def get_image_label():
     
     return flask.jsonify(img_label)
 
-@app.route('/get_image_path', methods=['GET'])
-def get_image_path():
-    
-    return flask.jsonify(data_dict["path"])
-
-@app.route('/get_image_url', methods=['GET', 'POST'])
-def get_image_url():
+@app.route('/get_image_score', methods=['GET', 'POST'])
+def get_image_score():
 
     selected_ids = None
     if request.method == 'POST':
@@ -181,14 +115,47 @@ def get_image_url():
             print("ERROR", e)
     
     if selected_ids is not None:
-        img_url = [img_url_data[i] for i in selected_ids]
+        label_list = [i for i in selected_ids]
+        img_score = scores[label_list]
     else:
-        img_url = img_url_data.copy() 
+        img_score = scores.copy() 
     
+    return flask.jsonify(img_score)
+
+@app.route('/get_image_path', methods=['GET'])
+def get_image_path():
+    
+    return flask.jsonify(data_dict["path"])
+
+@app.route('/get_image_url/<current_city>/', methods=['GET', 'POST'])
+def get_image_url(current_city):
+
+    selected_ids = None
+
+    if request.method == 'GET':
+        if current_city!="all" and current_city is not None:
+            img_url = [img for img in img_url_data if current_city in img].copy()
+        else:
+            img_url = img_url_data.copy()
+
+    if request.method == 'POST':
+        try:
+            selected_ids =  request.get_json()["selected_ids"]
+            print("Selected samples", selected_ids)
+            if selected_ids is not None:
+                img_url = [img_url_data[i] for i in selected_ids]
+            else:
+                if current_city!="all" and current_city is not None:
+                    img_url = [img for img in img_url_data if current_city in img].copy()
+                else:
+                    img_url = img_url_data.copy()
+        except Exception as e:
+            print("ERROR", e)
+
     return flask.jsonify(img_url)
 
-@app.route('/image/<img_name>', methods=['GET'])
-@app.route('/image/<city>/<img_name>', methods=['GET'])
+@app.route('/image/<img_name>/', methods=['GET'])
+@app.route('/image/<city>/<img_name>/', methods=['GET'])
 def get_image(img_name, city="Rio De Janeiro"):
 
     name_file = "images/painting.jpg"
@@ -201,51 +168,107 @@ def get_image(img_name, city="Rio De Janeiro"):
 
     return redirect(url_for("static", filename=name_file))
 
+def apply_max_pooling(images, pool_size=(4, 4)):
+    pool_images = []
+    for img in images:
+        #pooled_img = np.array([maximum_filter(ch, size=pool_size) for ch in img])
+        #pooled_img = maximum_filter(img, size=pool_size)
+        pooled_img = np.array([block_reduce(channel, pool_size, np.max) for channel in img])
+        #pooled_img = np.max(img, axis=(1, 2))
+        pool_images.append(pooled_img)
+    return np.array(pool_images)
 
-@app.route('/umap_clustering', methods=['GET','POST'])
-def get_umap_clusters():
+def get_umap_2D_projection(images, type_data="features"):
 
-    current_feature = "image"
+    print("Input", images.shape)
+    if type_data=="features":
+        flattened_images = images.copy()
+        flattened_images = (flattened_images - np.mean(flattened_images, axis=0)) / np.std(flattened_images, axis=0)
+    else:
+        #images_pooled = block_reduce(images, pool_size, np.max)
+        images_pooled = apply_max_pooling(images)
+        flattened_images = images_pooled.reshape(images_pooled.shape[0], -1)
+        flattened_images = flattened_images / 255.0
+    
+    print("Flatten", flattened_images.shape)
+    # Standardize the pooled activations
+    #scaler = StandardScaler()
+    #scaled_activations = scaler.fit_transform(flattened_images)
+        
+    # Apply UMAP for 2D projection
+    umap = UMAP(n_components=2)
+    embedding = umap.fit_transform(flattened_images)
+    print("results embedding:", embedding.shape)
+    projection = embedding.tolist()
+    
+    return projection
 
-    if request.method == 'POST':
-        try:
-            current_city =  request.get_json()["city"]
-            current_feature =  request.get_json()["feature"]
-            print("City selected")
-        except Exception as e:
-            print("ERROR", e)
+def get_tsne_2D_projection(images, type_data="features"):
 
-    return flask.jsonify({"projection": clustering_dict[f"{current_feature}_umap_clustering"]})
+    print("Input", images.shape)
+    if type_data=="features":
+        flattened_images = images.copy()
+        flattened_images = (flattened_images - np.mean(flattened_images, axis=0)) / np.std(flattened_images, axis=0)
+    else:
+        flattened_images = images.reshape(images.shape[0], -1)
+        flattened_images = flattened_images / 255.0
 
-@app.route('/pca_clustering', methods=['GET','POST'])
-def get_pca_clusters():
+    print("Flatten", flattened_images.shape)
+    # Normalize the flattened images to have zero mean and unit variance (optional but recommended)
 
-    current_feature = "image"
+    # Apply t-SNE to the normalized flattened images
+    tsne = TSNE(n_components=2)  # You can adjust the number of components as needed
+    tsne_images = tsne.fit_transform(flattened_images)
+    print("results embedding:", tsne_images.shape)
+    projection = tsne_images.tolist()
+    
+    return projection
 
-    if request.method == 'POST':
-        try:
-            current_city =  request.get_json()["city"]
-            current_feature =  request.get_json()["feature"]
-            print("City selected")
-        except Exception as e:
-            print("ERROR", e)
+def get_pca_2D_projection(images, type_data="features"):
 
-    return flask.jsonify({"projection": clustering_dict[f"{current_feature}_pca_clustering"]})
+    print("Input", images.shape)
+    if type_data=="features":
+        flattened_images = images.copy()
+        flattened_images = (flattened_images - np.mean(flattened_images, axis=0)) / np.std(flattened_images, axis=0)
+    else:
+        flattened_images = images.reshape(images.shape[0], -1)
+        flattened_images = flattened_images / 255.0
+    
+    print("Flatten", flattened_images.shape)
+    pca = PCA(n_components=2)  # Specify the desired number of components
+    pca_images = pca.fit_transform(flattened_images)
+    
+    # Access the principal components
+    principal_components = pca.components_
 
-@app.route('/tsne_clustering', methods=['GET','POST'])
-def get_tsne_clusters():
+    # Access the explained variance ratio
+    explained_variance_ratio = pca.explained_variance_ratio_
+    print("results embedding:", pca_images.shape)
+    projection = pca_images.tolist()
+    return projection
 
-    current_feature = "image"
+@app.route('/clustering/<type_cluster>/', methods=['GET','POST'])
+def get_clusters(type_cluster="umap", current_city="Boston"):
+
+    current_feature = "features"
+    current_city = None
 
     if request.method == 'POST':
         try:
             current_city =  request.get_json()["city"]
             current_feature =  request.get_json()["features"]
-            print("City selected")
         except Exception as e:
             print("ERROR", e)
+        
+    if current_city!="all" and current_city is not None:
+        indexes = [index for index, img in enumerate(img_url_data) if current_city in img].copy()
+    else:
+        indexes = list(range(len(img_url_data))).copy()
 
-    return flask.jsonify({"projection": clustering_dict[f"{current_feature}_tsne_clustering"]})
+    current_projection = clustering_dict[f"{current_feature}_{type_cluster}_clustering"].copy()
+    projection = [ proj for index, proj in enumerate(current_projection) if index in indexes ]
+
+    return flask.jsonify({"projection": projection})
 
 '''
 In the main, before running the server, run clustering, store results in variables a2_clustering, a3_clustering, a4_clustering
@@ -253,16 +276,16 @@ In the main, before running the server, run clustering, store results in variabl
 if __name__=='__main__':
 
     print("Clustering UMAp ...")
-    clustering_dict["image_umap_clustering"] = get_umap_2D_projection(images, type_data="images")
-    clustering_dict["feature_umap_clustering"] = get_umap_2D_projection(features, type_data="features")
+    #clustering_dict["images_umap_clustering"] = get_umap_2D_projection(images, type_data="images")
+    clustering_dict["features_umap_clustering"] = get_umap_2D_projection(features, type_data="features")
 
     print("Clustering PCA ...")
-    clustering_dict["image_pca_clustering"] = get_pca_2D_projection(images, type_data="images")
-    clustering_dict["feature_pca_clustering"] = get_pca_2D_projection(features, type_data="features")
+    #clustering_dict["images_pca_clustering"] = get_pca_2D_projection(images, type_data="images")
+    clustering_dict["features_pca_clustering"] = get_pca_2D_projection(features, type_data="features")
 
     print("Clustering t-SNE ...")
-    clustering_dict["image_tsne_clustering"] = get_tsne_2D_projection(images, type_data="images")
-    clustering_dict["feature_tsne_clustering"] = get_pca_2D_projection(features, type_data="features")
+    #clustering_dict["images_tsne_clustering"] = get_tsne_2D_projection(images, type_data="images")
+    clustering_dict["features_tsne_clustering"] = get_pca_2D_projection(features, type_data="features")
     
     print("Initializing server ...")
     app.run(port=PORT, use_reloader=False, threaded=True)
